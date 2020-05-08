1f635f2db590c20d8dd36198ddd4326a979e9d5f
katguerin <katguerin55@gmail.com>
2020-05-08 13:01:59 -0700

    at 9.3.5 error says there's an unexpected string but all my code looks right

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello Node!');
+const inquirer = require('inquirer');
+
+inquirer  
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message 'what is your name?'
+        }
+    ])
+.then(answers => console.log(answers));
+
+
+
+
+// const fs = require('fs');
+// const generatePage = require('./src');
+//...
+
+// const pageHTML = generatePage(portfolioData);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
